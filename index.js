d1128231484221398da3cf86dceda4ef75458f0c
Aviraj Kar <70070398+zenilist@users.noreply.github.com>
2023-08-22 22:55:34 -0600

    changed button UI and layout

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const container = document.querySelector(".grid-container");
-for(let i = 0; i < 256; i++){
-    const div = document.createElement('div');
-    div.classList.add("grid-square");
-    container.appendChild(div);
-    div.addEventListener("mouseover", function(){
-        div.style.backgroundColor = "black";
-    })
+const default_square_size = 16;
+const grid_height_width = 512;
+
+createGrid(default_square_size);
+
+function removeGrid() {
+    let lastChild = container.lastElementChild;
+    while (lastChild) {
+        container.removeChild(lastChild);
+        lastChild = container.lastElementChild;
+    }
 }
-// const grids = document.querySelectorAll(".grid-square")
+
+function createGrid(size = 16) {
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
+        div.style.height = `${grid_height_width / size}px`;
+        div.style.width = `${grid_height_width / size}px`;
+        div.classList.add("grid-square");
+        div.addEventListener("mouseover", function () {
+            div.style.backgroundColor = "black";
+        })
+        container.appendChild(div);
+    }
+}
+function resize() {
+    let size = prompt("Input new grid size (limit 1-100) ");
+
+    removeGrid();
+    createGrid(size);
+
+}
+
